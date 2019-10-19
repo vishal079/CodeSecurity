b366bd139bb0ea9ff1a4a1ceb3624f0ef7dff024
Vishal Joshi <joshi.vishal079@gmail.com>
2019-10-19 12:41:41 +0530

    Add files via upload

---
+++ DataCall.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApp1
+{
+    public class DataCall
+    {
+        EmployeeDA da = new EmployeeDA();
+        
+        public DataTable GetData()
+        {
+            DataTable dt = new DataTable();
+            string str= "select * from employee where status=1";
+            da.query = str;
+            dt = da.ExecuteSql();
+            return dt;
+        }
+    }
+}
---
+++ EmployeeDA.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace TestConsoleApp1
+{
+    public class EmployeeDA
+    {
+        public string query { get; set; }
+        public DataTable ExecuteSql()
+        {
+            DataTable dt = new DataTable();
+            string Conn = @"data source=192.168.51.158;initial catalog=Employee;persist security info=True;user id=sa;password=Dr.Nstein;";
+
+            using (SqlConnection sqlConn = new SqlConnection(Conn))
+            {
+                try
+                {
+                    using (SqlCommand sqlCmd = new SqlCommand())
+                    {
+                        sqlCmd.CommandText = string.Format("{0}", query);
+                        sqlCmd.Connection = sqlConn;
+                        sqlCmd.CommandType = CommandType.Text;
+                        sqlConn.Open();
+
+                        using (SqlDataAdapter sqlAdapter = new SqlDataAdapter(sqlCmd))
+                        {
+                            sqlAdapter.Fill(dt);
+                        }
+
+                        sqlCmd.Clone();
+                        sqlCmd.Dispose();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    sqlConn.Close();
+                    throw ex;
+                }
+                finally
+                {
+                    sqlConn.Dispose();
+                    sqlConn.Close();
+                }
+
+            }
+
+
+            return dt;
+        }
+    }
+}
